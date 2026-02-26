a30c5d48439813f040da6d48602c5d0d7f1958ed
root <wlsdn1232580@gmail.com>
2026-02-26 15:13:20 +0900

    [update] 문서 및 파일 정리

--- .tmp-gemini/package/dist/src/generated/git-commit.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @license
- * Copyright 2026 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-export declare const GIT_COMMIT_INFO = "1e3b992f6";
-export declare const CLI_VERSION = "0.30.0";
